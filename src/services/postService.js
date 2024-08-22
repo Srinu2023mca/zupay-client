af4dcf7cfe9fdc0c2286ef935e85af1f1da49d0e
Srinu2023mca <132866061+Srinu2023mca@users.noreply.github.com>
2024-08-22 22:16:29 +0530

    done

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/posts';
+// const API_URL = 'http://localhost:5000/posts';
+const API_URL = "https://zupay-server.onrender.com/posts"
 
 export const fetchPosts = () => axios.get(API_URL);
 export const fetchPostById = (id) => axios.get(`${API_URL}/${id}`);
